79f983b7ba1ee160998eb5328a31e04e37a98600
ali mert inal <mert.inal@gmail.com>
2023-05-19 11:14:51 +0200

    add bodyparser

--- repos/back-end/src/server.js
+++ repos/back-end/src/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {    
     res.send('Hello World!');
-}
-)
+})
+
+
+app.post('/', (req, res) => {    
+    res.send('my name is:' + req.body.name);
+})
+
+
 app.listen(3000, () => {    
     console.log('Example app listening on port 3000!');
 } 
