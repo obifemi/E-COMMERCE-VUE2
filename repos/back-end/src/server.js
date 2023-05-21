227a418199ec57a91a6aee63945963d49c03349e
ali mert inal <mert.inal@gmail.com>
2023-05-21 22:28:12 +0200

    add delete mongodb

--- repos/back-end/src/server.js
+++ repos/back-end/src/server.js
@@ -152,10 +152,25 @@ app.post('/api/users/:userId/cart', async(req, res) => {
   });
   
 
-  app.delete('/api/users/:userId/cart/:productId', (req, res) => {
-    const { productId } = req.params;
-    cartItems = cartItems.filter(product => product.id !== productId);
+  app.delete('/api/users/:userId/cart/:productId', async(req, res) => {
+    const { userId, productId } = req.params;
+    const client = await MongoClient.connect(
+      'mongodb://localhost:27017',
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+    const db = client.db('vue-db');
+  
+    await db.collection('users').updateOne({ id: userId }, {
+      $pull: { cartItems: productId },
+    });
+    const user = await db.collection('users').findOne({ id: userId });
+    const products = await db.collection('products').find({}).toArray();
+    const cartItemIds = user.cartItems;
+    const cartItems = cartItemIds.map(id =>
+      products.find(product => product.id === id));
+  
     res.status(200).json(cartItems);
+    client.close();
   });
 
 
