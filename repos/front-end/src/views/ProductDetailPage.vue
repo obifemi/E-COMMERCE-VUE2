5585e061dcf583aa8791f039b430e9bfe510ca02
ali mert inal <mert.inal@gmail.com>
2023-05-26 18:56:39 +0200

    add pro detail

--- repos/front-end/src/views/ProductDetailPage.vue
+++ repos/front-end/src/views/ProductDetailPage.vue
@@ -1,37 +1,41 @@
 <template>
-    <div id="page-wrap" v-if="product">
-        <div id="img-wrap">
-            <img :src="product.imageUrl" alt="product.name">
-        </div>
-        <div id="product-details">
-            <h1>{{product.name}}</h1>
-            <p id="price">{{product.price}}</p>
-            <p>Average Rating: {{ product.averageRating }}</p>
-            <button id="add-to-cart">Add to Cart</button>
-            <h4>Description:</h4>
-            <p>{{product.description}}</p>
-        </div>
+  <div id="page-wrap" v-if="product">
+    <div id="img-wrap">
+      <img :src="'/' + product.imageUrl"/>
     </div>
-    <NotFoundPage v-else/>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">${{ product.price }}</h3>
+      <p>Average rating: {{ product.averageRating }}</p>
+      <button id="add-to-cart">Add to Cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+  <NotFoundPage v-else/>
 </template>
 
 <script>
-import { products } from '../fake-data.js';
-import NotFoundPage from './NotFoundPage.vue';
-export default{
+import axios from 'axios';
+import NotFoundPage from './NotFoundPage';
+
+export default {
     name: 'ProductDetailPage',
-    data(){
-        return{
-            
-            product: products.find((p)=>p.id === this.$route.params.id)
-       
-        }
-    },
     components: {
-        NotFoundPage
+      NotFoundPage,
     },
-    
-}
+    data() {
+      return {
+        product: {},
+      };
+    },
+    async created() {
+      const result = await axios.get(`/api/products/${this.$route.params.id}`);
+      const product = result.data;
+      console.log(product);
+      this.product = product;
+    }
+};
 </script>
 
 <style scoped>
